15a30ba7b87fa17e4af4a49989c799017d56fec9
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2023-06-01 13:12:47 +0530

    Update CreateGroup.js
    removed duplicates

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -55,30 +55,8 @@ const CreateGroup = ({ setFieldValue, values }) => {
             </div>
           )}
           <Field
-          
-
-            onChange={(e) => {
-
-            const file = e.target.files[0];
-            const reader = new FileReader();
-
-             reader.readAsDataURL(file);
-            reader.onload = function (e) {
-             
-             setImg(e.target.result)
-
-            };
-
-           setFieldValue("proflie", Img);
-
-            
-          }}
-
-
-            
-
-            type="file"
-            name="groups.profile"
+type="file"
+            name="groups.profile"   
             id="profile"
             value={""}
             hidden
