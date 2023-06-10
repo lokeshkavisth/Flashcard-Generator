9e6ac381d4c2ddf049a6bb9b06e09edd3bbf04f8
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2023-06-10 13:35:00 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+// This code imports the React, ReactDOM, and App components.
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+
+// This code imports the Provider and store components from react-redux.
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+// This code imports the BrowserRouter component from react-router-dom.
+import { BrowserRouter } from "react-router-dom";
+
+// This code renders the App component in the root element.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    {/* The BrowserRouter component manages the application's routing.
+      It allows users to navigate between different pages in the application. */}
+    <BrowserRouter>
+      {/* The Provider component provides the store variable to the application's components.
+        The store variable contains the application's state. */}
+      <Provider store={store}>
+        {/* The App component is the main component of the application.
+          It renders the application's UI. */}
         <App />
-      </BrowserRouter>
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
