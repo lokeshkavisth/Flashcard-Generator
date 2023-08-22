1ef0e4bdab9a0c7f76f4c17485b08210ccf6c71d
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-08-22 16:08:35 +0530

    [cleanup]: improve code readability

--- src/pages/MyFlashcard.jsx
+++ src/pages/MyFlashcard.jsx
@@ -1,5 +1,3 @@
-// myflashcard page that shows all flashcards
-
 import React from "react";
 import Card from "../components/ui/card/Card";
 import { NavLink } from "react-router-dom";
@@ -14,8 +12,6 @@ const MyFlashcard = () => {
       data-testid="container"
       role="main"
     >
-      {/* if flashcard state data is not empty then shows cards else shows an message */}
-
       {flashcards.length === 0 ? (
         <div className="flex flex-col items-center justify-center gap-4 text-center mx-auto">
           <p className="text-black text-2xl">You don't have any flashcards</p>
@@ -24,7 +20,6 @@ const MyFlashcard = () => {
             to={"/"}
             data-testid="navigation"
             name="Create Flashcard"
-            // role="navigation"
             className={
               "px-6 py-2 bg-red-500 rounded-md text-xl font-semibold text-white hover:bg-red-700 transition-all"
             }
--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,20 +1,11 @@
-// This function creates an action that creates a flashcard.
 export const createFlashcard = (formData) => {
-  // The `type` property of the action is set to `CREATE_FLASHCARD`.
-  // This indicates that the action is for creating a flashcard.
-  // The `payload` property of the action is set to the `formData` object.
-  // This object contains the data that is used to create the flashcard.
   return {
     type: "CREATE_FLASHCARD",
     payload: formData,
   };
 };
 
-// This function creates an action that deletes a flashcard.
 export const deleteFlashcard = (id) => {
-  // The `type` property of the action is set to `DELETE_FLASHCARD`.
-  // This indicates that the action is for deleting a flashcard.
-  // The `payload` property of the action is set to the `id` of the flashcard that is to be deleted.
   return {
     type: "DELETE_FLASHCARD",
     payload: id,
